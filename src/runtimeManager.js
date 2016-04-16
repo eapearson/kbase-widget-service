5efd486cc9ceec51b369b3086fccccb3af1cb3cd
Erik Pearson <eapearson@lbl.gov>
2016-04-16 09:11:42 -0700

    update to use 1.4.0 of the service clients, the only one now supported really need to get this runtime config into the cdn...

--- src/runtimeManager.js
+++ src/runtimeManager.js
@@ -37,7 +37,7 @@ define([
                         yaml: 'require-yaml/0.1.2/yaml',
                             
                         // kbase
-                        'kb_service': 'kbase-service-clients-js/1.2.4',
+                        'kb_service': 'kbase-service-clients-js/1.4.0',
                         'kb_common': 'kbase-common-js/1.5.4',
                         'kb_widget_service': 'kbase-widget-service/0.1.0'
                     },
